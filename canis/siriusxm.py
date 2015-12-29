4289b0bd661e6c9b27e182a14c5dc72e6d7289ca
Max Goedjen <max.goedjen@gmail.com>
2015-12-28 22:03:12 -0600

    Starting to fill out siriusxm.py

---
+++ canis/siriusxm.py
@@ -0,0 +1,20 @@
+class Channel(object):
+
+	def __init__(self, name, identifier):
+		self.name = name
+		self.identifier = identifier
+
+class Song(object):
+
+	def __init__(self, title, artist);
+		self.title = title
+		self.artist = artist
+
+def get_channel_list():
+	pass
+
+def get_currently_playing(channel_identifier):
+	pass
+
+if __name__ == '__main__':
+	print(get_channel_list())
