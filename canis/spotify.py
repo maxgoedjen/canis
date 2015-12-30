52fdc50d4f663ea44ead1a98e906f1ea291f11e3
Max Goedjen <max.goedjen@gmail.com>
2015-12-30 17:31:47 -0600

    Return URI not id

--- canis/spotify.py
+++ canis/spotify.py
@@ -5,11 +5,10 @@ from canis.song import Song
 def id_for_song(song):
 	album_subq = '%20album:{}'.format(song.album) if song.album else ''
 	query = 'q=track:{}%20artist:{}{}&type=track'.format(song.title, song.artist, album_subq)
-	print 'https://api.spotify.com/v1/search?{}'.format(query)
 	r = requests.get('https://api.spotify.com/v1/search?{}'.format(query))
 	json = r.json()
 	track = json['tracks']['items'][0]
-	return track['id']
+	return track['uri']
 
 def playlist_id_for_name(name):
 	pass
