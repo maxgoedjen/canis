478e92a3323ab177c821fc4f4b6d4578ab40875c
Max Goedjen <max.goedjen@gmail.com>
2015-12-30 20:01:18 -0600

    Add oauth setup flow

--- canis/oauth.py
+++ canis/oauth.py
@@ -1,11 +1,68 @@
-from flask import Flask, request
+from os import environ
+from urllib import urlencode
+from datetime import datetime, timedelta
+
+from flask import Flask, request, redirect
+from flask_oauth import OAuth
+import requests
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
+SPOTIFY_CLIENT_ID = environ.get('CANIS_SPOTIFY_API_CLIENT_ID')
+SPOTIFY_SECRET = environ.get('CANIS_SPOTIFY_API_SECRET')
+SPOTIFY_CALLBACK = environ.get('CANIS_SPOTIFY_API_CALLBACK', 'http://127.0.0.1:5000/callback/')
+
+access_token = None
+refresh_token = None
+expiration = None
+
+@app.route('/login')
+def login():
+    args = {
+        'client_id': SPOTIFY_CLIENT_ID,
+        'response_type': 'code',
+        'redirect_uri': SPOTIFY_CALLBACK,
+        'scope': 'playlist-read-private playlist-modify-private playlist-modify-public',
+    }
+    arg_str = urlencode(args)
+    url = 'https://accounts.spotify.com/authorize?{}'.format(arg_str)
+    return redirect(url)
+
+@app.route('/callback/')
+def callback():
+    args = {
+        'code': request.args['code'],
+        'grant_type': 'authorization_code',
+        'redirect_uri': SPOTIFY_CALLBACK,
+        'client_id': SPOTIFY_CLIENT_ID,
+        'client_secret': SPOTIFY_SECRET
+    }
+    r = requests.post('https://accounts.spotify.com/api/token', data=args)
+    resp = r.json()
+    store_token_response(resp)
     shutdown_server()
-    return "Hello World!"
+    return "You're good to go"
+
+def refresh():
+    args = {
+        'refresh_token': refresh_token,
+        'grant_type': 'refresh_token',
+        'client_id': SPOTIFY_CLIENT_ID,
+        'client_secret': SPOTIFY_SECRET
+    }
+    r = requests.post('https://accounts.spotify.com/api/token', data=args)
+    resp = r.json()
+    store_token_response(resp)
+
+def store_token_response(resp):
+    global access_token
+    global refresh_token
+    global expiration
+    access_token = resp['access_token']
+    if resp.get('refresh_token'):
+        refresh_token = resp['refresh_token']
+    expiration = datetime.now() + timedelta(seconds=int(resp['expires_in']))
+    print (access_token, refresh_token, expiration)
 
 def shutdown_server():
     func = request.environ.get('werkzeug.server.shutdown')
