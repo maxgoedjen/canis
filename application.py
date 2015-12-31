a806d55b7cb2c554357895ca441f30c906aa1fc1
Max Goedjen <max.goedjen@gmail.com>
2015-12-30 22:12:21 -0600

    Restructure error handling a bit

--- application.py
+++ application.py
@@ -1,12 +1,20 @@
+from time import sleep
+from datetime import datetime
 from canis import siriusxm, spotify, oauth
 
 def main():
-    try:
-        current = siriusxm.get_currently_playing('siriusxmu')
-        spotify_id = spotify.id_for_song(current)
-        print(current, spotify_id)
-    except Exception, e:
-        print "Error {}".format(e)
+    channels = ['siriusxmu', 'altnation']
+    while True:
+        if oauth.expiration > datetime.utcnow():
+            oauth.refresh()
+        for channel in channels:
+            try:
+                current = siriusxm.get_currently_playing(channel)
+                spotify_id = spotify.id_for_song(current)
+                print '{} - {}'.format(current, spotify_id)
+            except Exception, e:
+                print "Error {}".format(e)
+        sleep(60)
 
 if __name__ == "__main__":
     oauth.app.run()
