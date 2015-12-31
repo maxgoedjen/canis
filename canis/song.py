06661c83039a499d59b47e64207b99e03896472c
Max Goedjen <max.goedjen@gmail.com>
2015-12-31 00:36:55 -0600

    Fix eq and ne

--- canis/song.py
+++ canis/song.py
@@ -8,5 +8,10 @@ class Song(object):
     def __repr__(self):
         return '{} by {} ({})'.format(self.title, self.artist, self.album or 'No Album')
 
-    def __eq__(self, other): 
+    def __eq__(self, other):
+    	if not other:
+    		return False
         return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+    	return not (self == other)
