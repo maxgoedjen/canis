f58b88699240017cc5d99af93806269ff09f7032
Max Goedjen <max.goedjen@gmail.com>
2015-12-30 23:30:31 -0600

    Add equality to song type

--- canis/song.py
+++ canis/song.py
@@ -1,9 +1,12 @@
 class Song(object):
 
-	def __init__(self, title, artist, album):
-		self.title = title
-		self.artist = artist
-		self.album = album
+    def __init__(self, title, artist, album):
+        self.title = title
+        self.artist = artist
+        self.album = album
 
-	def __repr__(self):
-		return '{} by {} ({})'.format(self.title, self.artist, self.album or 'No Album')
+    def __repr__(self):
+        return '{} by {} ({})'.format(self.title, self.artist, self.album or 'No Album')
+
+    def __eq__(self, other): 
+        return self.__dict__ == other.__dict__
