a2b3f0b1bc609ad8ad7e893aedd2878e5496ed04
Max Goedjen <max.goedjen@gmail.com>
2015-12-30 22:22:26 -0600

    Add not found exception to spotify

--- canis/spotify.py
+++ canis/spotify.py
@@ -2,12 +2,20 @@ import requests
 
 from canis.song import Song
 
+class NotFound(Exception):
+	
+	def __init__(self, song):
+		self.song = song
+
 def id_for_song(song):
 	album_subq = '%20album:{}'.format(song.album) if song.album else ''
 	query = 'q=track:{}%20artist:{}{}&type=track'.format(song.title, song.artist, album_subq)
 	r = requests.get('https://api.spotify.com/v1/search?{}'.format(query))
 	json = r.json()
-	track = json['tracks']['items'][0]
+	try:
+		track = json['tracks']['items'][0]
+	except Exception:
+		raise NotFound(song)
 	return track['uri']
 
 def playlist_id_for_name(name):
